14ae3f63b4e13b7b8f9d0060df15328586858634
Maitann <hiagomaitan@gmail.com>
2023-06-16 00:27:28 -0300

    FEAT: defining route layout

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,21 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Users\\Hiago\\Documents\\Rocketseat\\NLW SpaceTime\\web\\src\\app\\layout.tsx": [
+      "C:\\Users\\Hiago\\Documents\\Rocketseat\\NLW SpaceTime\\web\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Roboto_Flex\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-roboto\"}],\"variableName\":\"roboto\"}",
+      "C:\\Users\\Hiago\\Documents\\Rocketseat\\NLW SpaceTime\\web\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Bai_Jamjuree\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":\"700\",\"variable\":\"--font-bai-jamjuree\"}],\"variableName\":\"baiJamjuree\"}",
+      "C:\\Users\\Hiago\\Documents\\Rocketseat\\NLW SpaceTime\\web\\src\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\Hiago\\Documents\\Rocketseat\\NLW SpaceTime\\web\\src\\app\\page": [
+      "C:\\Users\\Hiago\\Documents\\Rocketseat\\NLW SpaceTime\\web\\src\\app\\globals.css",
+      "C:\\Users\\Hiago\\Documents\\Rocketseat\\NLW SpaceTime\\web\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Roboto_Flex\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-roboto\"}],\"variableName\":\"roboto\"}",
+      "C:\\Users\\Hiago\\Documents\\Rocketseat\\NLW SpaceTime\\web\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Bai_Jamjuree\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":\"700\",\"variable\":\"--font-bai-jamjuree\"}],\"variableName\":\"baiJamjuree\"}"
+    ],
+    "C:\\Users\\Hiago\\Documents\\Rocketseat\\NLW SpaceTime\\web\\src\\app\\memories\\new\\page": [
+      "C:\\Users\\Hiago\\Documents\\Rocketseat\\NLW SpaceTime\\web\\src\\app\\globals.css",
+      "C:\\Users\\Hiago\\Documents\\Rocketseat\\NLW SpaceTime\\web\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Roboto_Flex\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-roboto\"}],\"variableName\":\"roboto\"}",
+      "C:\\Users\\Hiago\\Documents\\Rocketseat\\NLW SpaceTime\\web\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Bai_Jamjuree\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":\"700\",\"variable\":\"--font-bai-jamjuree\"}],\"variableName\":\"baiJamjuree\"}"
+    ]
+  }
+}
